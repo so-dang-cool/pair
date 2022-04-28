dd6847533273055a1d97285f9c1df53b49c94be4
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-04-28 00:05:56 -0700

    Fix the test and deploy boilerplate

---
+++ src/test/java/so/dang/cool/PairTest.java
@@ -0,0 +1,12 @@
+package so.dang.cool;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PairTest {
+    @Test
+    public void testApp() {
+        assertTrue(1+1 == 2);
+    }
+}
